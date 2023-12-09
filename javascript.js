d725320c992b3692e2299813d0f3424ed5ae9359
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2023-12-09 23:27:31 +0100

    Add numbers being displayed with click of button

--- javascript.js
+++ javascript.js
@@ -27,6 +27,54 @@ function operate (operator, a, b) {
     if (operator === divide) operationDivide(a, b);
 }
 
+// declare function that populates the display
+function populateDisplay(button) {
+    // console.log(typeof button.id);
+    // console.log(button.textContent)
+    switch (button.id) {
+        case "btnNum1":
+        case "btnNum2":
+        case "btnNum3":
+        case "btnNum4":
+        case "btnNum5":
+        case "btnNum6":
+        case "btnNum7":
+        case "btnNum8":
+        case "btnNum9":
+        case "btnNum0":
+            console.log(button.id);
+            display.textContent = "";
+            display.textContent = button.textContent;
+            break;
+        case "btnAdd":
+        case "btnSubtract":
+        case "btnMultiply":
+        case "btnDivide":
+            // something with the operator
+            console.log(button.id)
+            break;
+        case "btnDot":
+            // add dot
+            console.log(button.id)
+            break;
+        case "btnEquals":
+            // calculate
+            console.log(button.id)
+            break;
+        case "btnClear":
+            // clear
+            console.log(button.id)
+            break;
+        case "btnDelete":
+            // delete
+            console.log(button.id)
+            break;
+        default:
+            console.log("Invalid button ID");
+            break;
+    }
+}
+
 // variables for two numbers and operator
 operator = "";
 a = 7;
@@ -35,11 +83,13 @@ b = 5;
 // select all the buttons with the class name "btn"
 const buttons = document.querySelectorAll(".btn");
 
+// select display
+const display = document.querySelector("#display");
+
 // loop through each button and add a click event listener
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
-    // populateDisplay();
     console.log("You clicked a button");
-    console.log(button);
+    populateDisplay(button);
   });
 });
