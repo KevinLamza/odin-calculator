356079893313b5f77002cba1b7b86c427117252b
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2023-12-09 22:14:45 +0100

    Add function "operate"
    Takes two numbers and an operator as input and calls
    the according math operation function.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,17 @@
-function add (a, b) {
+// declare four functions for four basic math operations
+function operationAdd (a, b) {
     return a + b;
 }
 
-function subtract (a, b) {
+function operationSubtract (a, b) {
     return a - b;
 }
 
-function multiply (a, b) {
+function operationMultiply (a, b) {
     return a * b;
 }
 
-function divide (a, b) {
+function operationDivide (a, b) {
     if (b === 0) {
         return "You can't divide by zero!"
     } else {
@@ -18,9 +19,18 @@ function divide (a, b) {
     }
 }
 
+// declare function that calls one of the basic four math operations
+function operate (operator, a, b) {
+    if (operator === add) operationAdd(a, b);
+    if (operator === subtract) operationSubtract(a, b);
+    if (operator === multiply) operationMultiply(a, b);
+    if (operator === divide) operationDivide(a, b);
+}
+
+// variables for two numbers and operator
+operator = "";
 a = 7;
 b = 5;
-operator = "";
 
 alert(add(a, b));
 alert(subtract(a, b));
